a79d3a7d15447c019a9c551af826b67b0932f592
Mackenzie Child <mackenzie.child@gmail.com>
2023-10-31 16:23:09 -0400

    Update theme toggle to use cookies by default + some refactoring

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -7,10 +7,55 @@ document.addEventListener("DOMContentLoaded", function () {
   const toLightModeButtonMobile = document.getElementById("toLightModeMobile");
   const toDarkModeButtonMobile = document.getElementById("toDarkModeMobile");
 
-  const setTheme = (theme) => {
+  let currentDomain =
+    window.location.hostname === "wfu3.webflow.io"
+      ? "wfu3.webflow.io"
+      : ".webflow.com";
+
+  // Test to see if cookies are enabled
+  function checkCookieEnabled() {
+    // Try to set a test cookie
+    document.cookie =
+      "testcookie=1; expires=Wed, 01-Jan-2070 00:00:01 GMT; path=/";
+
+    // Try to get the test cookie
+    const isEnabled = document.cookie.indexOf("testcookie") !== -1;
+
+    // Delete the test cookie
+    document.cookie =
+      "testcookie=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/";
+
+    return isEnabled;
+  }
+
+  const isCookieEnabled = checkCookieEnabled();
+
+  // Function to get the current theme
+  const getThemeStorage = () =>
+    Cookies.get("wfu-theme") || localStorage.getItem("theme");
+
+  // Function to set the theme
+  const setThemeStorage = (value) => {
+    try {
+      Cookies.set("wfu-theme", value, {
+        expires: 365,
+        domain: currentDomain
+      });
+    } catch (e) {
+      localStorage.setItem("theme", value);
+    }
+  };
+
+  // Check if Theme is set in local storage
+  // if true && cookies are enabled, swap it to use cookies instead
+  const localStorageTheme = localStorage.getItem("theme");
+  if (localStorageTheme && isCookieEnabled) {
+    setThemeStorage(localStorageTheme);
+    localStorage.removeItem("theme"); // Remove from local storage
+  }
+
+  const updateButtons = (theme) => {
     const screenWidth = window.innerWidth;
-    document.documentElement.setAttribute("data-theme", theme);
-    localStorage.setItem("theme", theme);
 
     // For desktop
     if (screenWidth > 768) {
@@ -40,40 +85,35 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 
-  const attachListeners = () => {
-    toLightModeButton.addEventListener("click", () => {
-      setTimeout(() => {
-        setTheme("light");
-      }, 380);
-    });
+  const setTheme = (theme) => {
+    document.documentElement.setAttribute("data-theme", theme);
+    setThemeStorage(theme);
+    updateButtons(theme);
+  };
 
-    toDarkModeButton.addEventListener("click", () => {
+  // Function to handle theme click events
+  const handleThemeClick = (element, theme) => {
+    element.addEventListener("click", () => {
       setTimeout(() => {
-        setTheme("dark");
-      }, 380);
-    });
-
-    toLightModeButtonMobile.addEventListener("click", () => {
-      setTimeout(() => {
-        setTheme("light");
-      }, 380);
-    });
-
-    toDarkModeButtonMobile.addEventListener("click", () => {
-      setTimeout(() => {
-        setTheme("dark");
+        setTheme(theme);
       }, 380);
     });
   };
 
-  // Initialize theme and add event listeners based on initial viewport
-  const savedTheme = localStorage.getItem("theme") || "dark";
-  setTheme(savedTheme);
+  const attachListeners = () => {
+    handleThemeClick(toLightModeButton, "light");
+    handleThemeClick(toDarkModeButton, "dark");
+    handleThemeClick(toLightModeButtonMobile, "light");
+    handleThemeClick(toDarkModeButtonMobile, "dark");
+  };
+
+  // Initialize
+  setTheme(getThemeStorage() || "dark");
   attachListeners();
 
-  // Re-attach appropriate event listeners on resize
+  // Listen for resize changes & update buttons accordingly
   window.addEventListener("resize", () => {
-    const currentTheme = localStorage.getItem("theme") || "dark";
-    setTheme(currentTheme);
+    const currentTheme = getThemeStorage() || "dark";
+    updateButtons(currentTheme);
   });
 });
